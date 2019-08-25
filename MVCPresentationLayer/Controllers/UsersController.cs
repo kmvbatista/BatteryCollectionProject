df487048b467be7404d8d6dd9df676db6decafd0
kmvbatista <kennedymnovas@gmail.com>
2019-08-24 21:39:29 -0300

    mesma correçaõp

--- MVCPresentationLayer/Controllers/UsersController.cs
+++ MVCPresentationLayer/Controllers/UsersController.cs
@@ -1,48 +1,113 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using DataTypeObject;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using BusinessLogicalLayer;
 
-namespace MVCPresentationLayer.Controllers
+namespace WebPresentationLayer.Controllers
 {
-    [Route("api/[controller]")]
+
+    [Route("api/users")]
     [ApiController]
     public class UsersController : ControllerBase
     {
-        // GET: api/Users
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly UserBLL userBLL;
+
+        public UsersController(UserBLL _userBLL)
         {
-            return new string[] { "value1", "value2" };
+            userBLL = _userBLL;
+        }
+
+        public IActionResult Login(string email, string password)
+        { 
+            User userFound= userBLL.Authenticate(email, password);
+            if(userFound != null)
+            {
+                return new ObjectResult(userFound);
+            }
+            return NotFound();
         }
 
         // GET: api/Users/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public IActionResult GetById(int id)
         {
-
-            return "value";
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var userFound = userBLL.Find(id);
+            if (userFound == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(userFound);
         }
 
         // POST: api/Users
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Create([FromBody] User _user)
         {
+            if (_user == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                userBLL.Add(_user);
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
+            return CreatedAtRoute("GetUser", new { id = _user.Id }, _user);
         }
 
         // PUT: api/Users/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] User user)
         {
+            if (user == null || user.Id != id)
+            {
+                return BadRequest();
+            }
+
+            User userFound = userBLL.Find(id);
+            if (userFound == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                userBLL.Update(user);
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
+            return new NoContentResult();
         }
 
-        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var user = userBLL.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                userBLL.Remove(id);
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
+
+            return new NoContentResult();
         }
     }
 }
