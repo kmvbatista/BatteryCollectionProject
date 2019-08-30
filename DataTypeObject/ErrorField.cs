c9cda3599c3410e3350f307159e4d74009f41645
Cursos Oficiais Microsoft <moc@proway.treina>
2019-08-30 17:17:33 -0300

    bll validates

---
+++ DataTypeObject/ErrorField.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTypeObject
+{
+    public class ErrorField
+    {
+        public string PropertyName { get; set; }
+        public string Error { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
