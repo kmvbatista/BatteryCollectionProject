71e3546c1a0e400eadf0162eb1df948d531d26d3
kmvbatista <kennedymnovas@gmail.com>
2019-09-28 00:20:51 -0300

    place latitude longitude

--- DataTypeObject/Place.cs
+++ DataTypeObject/Place.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
