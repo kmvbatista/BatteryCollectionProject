0ab3707c750f825d889eb15f51b27f369bbe9d00
kmvbatista <kennedymnovas@gmail.com>
2019-10-07 01:21:23 -0300

    content-title

---
+++ DataAccessLayer/Migrations/20191007042031_content-title.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class contenttitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ask",
+                table: "AskAndAnswers",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Answer",
+                table: "AskAndAnswers",
+                newName: "Content");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "AskAndAnswers",
+                newName: "Ask");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "AskAndAnswers",
+                newName: "Answer");
+        }
+    }
+}
--- DataTypeObject/AskAndAnswers.cs
+++ DataTypeObject/AskAndAnswers.cs
@@ -3,7 +3,7 @@ namespace DataTypeObject
     public class AskAndAnswers
     { 
       public int Id { get; set; }
-      public string Ask { get; set; }
-      public string Answer { get; set; }
+      public string Title { get; set; }
+      public string Content { get; set; }
     }
 }
